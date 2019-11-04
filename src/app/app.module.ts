8d19316f08ad371a992b087b768157640e34c2c4
Swarna <swarnakishoree@gmail.com>
2019-11-04 16:29:52 -0600

    created a new buttons view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BootstrapModalComponent } from './bootstrap-modal/bootstrap-modal.component';
-import { LoaderModule } from './loader/loader.module';
-import { ButtonModule } from './button/button.module';
+import { LoaderModule } from './modules/loader/loader.module';
+import { ButtonModule } from './modules/button/button.module';
+import { ButtonsViewComponent } from './components/buttons-view/buttons-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BootstrapModalComponent,
+    ButtonsViewComponent,
   ],
   imports: [
     BrowserModule,
